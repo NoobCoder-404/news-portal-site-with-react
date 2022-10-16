1e23bda0e6d24f2fbd03218a7558448c03005a77
Noob-H4cker <alsamiul123@gmail.com>
2022-10-16 16:26:41 +0600

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('daisyui')]
 };
