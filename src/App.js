dc7c11364758ae8dc64c3d2529c15a0342ba7a7b
Noob-H4cker <alsamiul123@gmail.com>
2022-10-19 12:36:02 +0600

    basic components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './Routes/Routes';
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 };
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAi-Bbaz5yyl97NEEKQW4u2vKgA6v3-cfU',
+  authDomain: 'dragon-news-b4da3.firebaseapp.com',
+  projectId: 'dragon-news-b4da3',
+  storageBucket: 'dragon-news-b4da3.appspot.com',
+  messagingSenderId: '1063804336385',
+  appId: '1:1063804336385:web:2283ad62c0862568484300'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
