e0eff849cf4a597a0ac4a705fe1f966da79480ad
Noob-H4cker <alsamiul123@gmail.com>
2022-10-19 19:44:48 +0600

    header complete

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ListGroup from 'react-bootstrap/ListGroup';
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitch,
+  FaTwitter,
+  FaWhatsapp
+} from 'react-icons/fa';
+import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
   return (
     <div>
-      <h1>Right Side Nav</h1>
+      <ButtonGroup vertical>
+        <Button className="mb-3 " variant="outline-primary">
+          <FaGoogle></FaGoogle> Login with Google
+        </Button>
+        <Button variant="outline-dark ">
+          <FaGithub /> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-3">
+        <h5>Find Us On</h5>
+        <ListGroup>
+          <ListGroup.Item className="mt-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mt-2">
+            <FaWhatsapp /> Whatsapp
+          </ListGroup.Item>
+          <ListGroup.Item className="mt-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mt-2">
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
+          <ListGroup.Item className="mt-2">
+            <FaTwitch /> Twitch
+          </ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div>
+        <BrandCarousel />
+      </div>
     </div>
   );
 };
